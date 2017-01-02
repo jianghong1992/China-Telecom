829af2c5b513e79532cfa6059b3a7bb2722bd7ed
Jianghong1992 <1376424033@qq.com>
2017-01-02 10:41:23 +0800

    init

---
+++ WAP/js/common.js
@@ -0,0 +1,21 @@
+$(function(){
+	$('#gotop').hide();
+	b();
+    $('#gotop').click(function(){
+        $(document).scrollTop(0);
+    });
+});
+
+$(window).scroll(function(e){
+    b();
+});
+
+function b(){
+    h = $(window).height();
+    t = $(document).scrollTop();
+    if(t > h){
+        $('#gotop').show();
+    }else{
+        $('#gotop').hide();
+    }
+}
